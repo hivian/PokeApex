b1ba9b9049aab6032b0ea15ccb21476b78e2e14e
Hugues Ivian <hugues@mention.com>
2020-03-24 19:40:14 +0100

    Base fragment improved

--- common/src/main/java/com/hivian/common/base/BaseFragment.kt
+++ common/src/main/java/com/hivian/common/base/BaseFragment.kt
@@ -1,25 +1,88 @@
 package com.hivian.common.base
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.FragmentNavigator
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
+import androidx.databinding.ViewDataBinding
 import com.google.android.material.snackbar.Snackbar
 import com.hivian.common.extension.setupSnackbar
 import com.hivian.common.navigation.NavigationCommand
 
-abstract class BaseFragment: Fragment() {
+abstract class BaseFragment<B : ViewDataBinding, M : BaseViewModel>(
+    @LayoutRes
+    private val layoutId: Int
+): Fragment() {
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        observeNavigation(getViewModel())
-        setupSnackbar(this, getViewModel().snackBarError, Snackbar.LENGTH_LONG)
+    lateinit var viewModel: M
+    lateinit var viewBinding: B
+
+    /**
+     * Called to Initialize view data binding variables when fragment view is created.
+     */
+    abstract fun onInitDataBinding()
+
+    /**
+     * Called to have the fragment instantiate its user interface view.
+     *
+     * @param inflater The LayoutInflater object that can be used to inflate any views in the fragment.
+     * @param container If non-null, this is the parent view that the fragment's UI should be
+     * attached to. The fragment should not add the view itself, but this can be used to generate
+     * the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed from a previous
+     * saved state as given here.
+     *
+     * @return Return the View for the fragment's UI, or null.
+     *
+     * @see Fragment.onCreateView
+     */
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewBinding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        viewBinding.lifecycleOwner = viewLifecycleOwner
+        return viewBinding.root
     }
 
-    abstract fun getViewModel(): BaseViewModel
+    /**
+     * Called to have the fragment instantiate its user interface view.
+     *
+     * @param view The view returned by onCreateView(LayoutInflater, ViewGroup, Bundle)}.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     * @see Fragment.onViewCreated
+     */
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onInitDataBinding()
+    }
+
+    /**
+     * Called when the fragment's activity has been created and this
+     * fragment's view hierarchy instantiated.
+     *
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     * @see Fragment.onActivityCreated
+     */
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        observeNavigation(getBaseViewModel())
+        setupSnackbar(this, getBaseViewModel().snackBarError, Snackbar.LENGTH_LONG)
+    }
+
+    abstract fun getBaseViewModel(): BaseViewModel
 
     // UTILS METHODS ---
 
@@ -42,4 +105,23 @@ abstract class BaseFragment: Fragment() {
      * [FragmentNavigatorExtras] mainly used to enable Shared Element transition
      */
     open fun getExtras(): FragmentNavigator.Extras = FragmentNavigatorExtras()
+
+    /**
+     * Return the [AppCompatActivity] this fragment is currently associated with.
+     *
+     * @throws IllegalStateException if not currently associated with an activity or if associated
+     * only with a context.
+     * @throws TypeCastException if the currently associated activity don't extend [AppCompatActivity].
+     *
+     * @see requireActivity
+     */
+    fun requireCompatActivity(): AppCompatActivity {
+        requireActivity()
+        val activity = requireActivity()
+        if (activity is AppCompatActivity) {
+            return activity
+        } else {
+            throw TypeCastException("Main activity should extend from 'AppCompatActivity'")
+        }
+    }
 }
